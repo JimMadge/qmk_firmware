5b6aaf82d48578315a4c0c844149ea4c7ec3f5a2
Jim Madge <jim@jmadge.com>
2023-08-12 14:53:39 +0100

    Add cmus-style media controls

--- keyboards/dz60/keymaps/jimmadge/keymap.c
+++ keyboards/dz60/keymaps/jimmadge/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
     _______, _______, KC_UP,   _______, _______, _______, _______, KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_PSCR, _______,
     _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______, _______, _______,
-    _______, _______, _______, ______,  _______, _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______,
+    _______, _______, KC_MPRV, ______,  KC_MPLY, KC_MSTP, KC_MNXT, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______,
     _______, _______, _______,                   TG(_CN),                            _______, _______, _______, _______
   ),
 
